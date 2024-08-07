0ffcee74e5e12c309958367419cf54fb8ebd33dc
Anubrata Chatterjee <chatterjeeanubrata386@gmail.com>
2024-08-07 12:36:13 +0530

    --Temporary hid the user freedom pagination feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
                         <input type="date" name="created_date" class="form-control"
                             value="{{ request('created_date') }}">
                     </div>
-                    <div class="col-md-4 mb-3">
+                    {{-- <div class="col-md-4 mb-3">
                         <select name="per_page" class="form-control" onchange="this.form.submit()">
                             @for ($i = 1; $i <= 20; $i++)
                                 <option value="{{ $i }}" {{ request('per_page') == $i ? 'selected' : '' }}>
@@ -50,7 +50,7 @@
                                 </option>
                             @endfor
                         </select>
-                    </div>
+                    </div> --}}
                 </div>
                 <button type="submit" class="btn btn-primary">Search</button>
             </form>
